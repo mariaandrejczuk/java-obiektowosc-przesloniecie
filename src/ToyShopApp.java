93f7ea21c4ea5cd1dbc4b7f620722e4a6f962b66
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-02-05 08:49:41 +0100

    #1 dodanie menu

--- src/ToyShopApp.java
+++ src/ToyShopApp.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ToyShopApp {
     public static void main(String[] args) {
-        //od 8:20
+
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+        int option;
+        do {
+            System.out.println("-------------Toy Shop-----------");
+            System.out.println("1 - add bear");
+            System.out.println("2 - add car");
+            System.out.println("3 - display all");
+            System.out.println("0 - stop");
+
+            option = scanner.nextInt();
+            scanner.nextLine(); // \n
+
+
+            switch (option) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        } while (option != 0);
+
+        scanner.close();
     }
 }
